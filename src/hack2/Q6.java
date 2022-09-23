be711da3795efe78b9136d49bc83560a302215a6
LakshmiipriyaaV <Lakshmipriya.V@ust.com>
2022-09-23 16:50:06 +0530

    Initial commit

---
+++ src/hack2/Q6.java
@@ -0,0 +1,18 @@
+package hack2;
+
+import java.util.ArrayList;
+
+class Q6 {
+     
+      public static void main(String[] args) {
+          ArrayList<String> list = new ArrayList<>();
+          list.add("A");
+          list.add("B");
+          list.add("C");
+          list.add("D");
+          System.out.println(list);
+          list.set(1, "E");
+          System.out.println(list);
+     
+      }
+    }
